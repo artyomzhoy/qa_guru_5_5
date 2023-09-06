2862ae2b5b850c77d226d8604ff871206e0cd89e
Artyom Samoshin <artyomzhoy@gmail.com>
2023-09-06 17:59:13 +0300

    add more pass/fail tests

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -4,3 +4,15 @@ def test_fail_1():
 
 def test_fail_2():
     assert False
+
+
+def test_fail_3():
+    assert False
+
+
+def test_fail_4():
+    assert False
+
+
+def test_fail_5():
+    assert False
--- tests/test_pass.py
+++ tests/test_pass.py
@@ -4,3 +4,37 @@ def test_pass_1():
 
 def test_pass_2():
     pass
+
+
+def test_pass_3():
+    pass
+
+
+def test_pass_4():
+    pass
+
+
+def test_pass_5():
+    pass
+
+
+def test_pass_6():
+    pass
+
+
+def test_pass_7():
+    pass
+
+
+def test_pass_8():
+    pass
+
+
+def test_pass_9():
+    pass
+
+
+def test_pass_10():
+    pass
+
+
